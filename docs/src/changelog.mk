80984b23c9041a027ea2a8400d591d70935af944
Darren Mulholland <darren@mulholland.xyz>
2018-05-02 17:39:50 +0100

    Update documentation

--- docs/src/changelog.mk
+++ docs/src/changelog.mk
@@ -3,6 +3,16 @@ title: Changelog
 ---
 
 
+### 1.3.0
+
+* Add a `restore` command for restoring inactive entries.
+
+* Add a `--deleted` flag to the `list` command for displaying inactive entries.
+
+* Add a confirmation step to the `purge` command.
+
+
+
 ### 1.2.0
 
 * Add a `url` command for copying an entry's url to the clipboard.

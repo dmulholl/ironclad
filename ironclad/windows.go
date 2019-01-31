68a1280411c172f2cda2b6561f1691c15d26a8d9
Darren Mulholland <dmulholl@tcd.ie>
2019-01-31 20:13:14 +0000

    Turn on ANSI support in the Windows console

---
+++ ironclad/windows.go
@@ -0,0 +1,20 @@
+// +build windows
+
+package main
+
+
+import (
+    "os"
+    "golang.org/x/sys/windows"
+)
+
+
+// Enable support for ANSI escape sequences in the Windows console.
+// Ref: https://stackoverflow.com/questions/39627348
+func init() {
+    var originalMode uint32
+    stdout := windows.Handle(os.Stdout.Fd())
+    windows.GetConsoleMode(stdout, &originalMode)
+    ansiMode := originalMode | windows.ENABLE_VIRTUAL_TERMINAL_PROCESSING
+    windows.SetConsoleMode(stdout, ansiMode)
+}

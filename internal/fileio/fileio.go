c53980e6d31233eeefa2a21e1ce11f23819685e1
Darren Mulholland <dmulholl@tcd.ie>
2024-02-18 00:12:46 +0000

    Add tests for internal/fileio

--- internal/fileio/fileio.go
+++ internal/fileio/fileio.go
@@ -6,7 +6,8 @@ package fileio
 import (
 	"bytes"
 	"compress/gzip"
-	"io/ioutil"
+	"fmt"
+	"io"
 	"os"
 
 	"github.com/dmulholl/ironclad/internal/ironcrypt"
@@ -20,12 +21,10 @@ const SaltLength = 32
 const PBKDFIterations = 100000
 
 // Load reads data from an encrypted file.
-func Load(filename, password string) (data []byte, err error) {
-
-	// Load the file content.
-	content, err := ioutil.ReadFile(filename)
+func Load(filename, password string) ([]byte, error) {
+	content, err := os.ReadFile(filename)
 	if err != nil {
-		return
+		return nil, fmt.Errorf("failed to read file: %w", err)
 	}
 
 	// Split the file content into its component parts.
@@ -38,39 +37,44 @@ func Load(filename, password string) (data []byte, err error) {
 	// Use the key to decrypt the ciphertext.
 	plaintext, err := aes.Decrypt(ciphertext, key)
 	if err != nil {
-		return
+		return nil, fmt.Errorf("failed to decrypt file: %w", err)
 	}
 
 	// Unzip the plaintext.
 	unzipper, err := gzip.NewReader(bytes.NewBuffer(plaintext))
 	if err != nil {
-		return
+		return nil, fmt.Errorf("failed to unzip plaintext: %w", err)
 	}
-	unzipped, err := ioutil.ReadAll(unzipper)
+	defer unzipper.Close()
+
+	unzipped, err := io.ReadAll(unzipper)
 	if err != nil {
-		return
+		return nil, fmt.Errorf("failed to unzip plaintext: %w", err)
 	}
-	unzipper.Close()
 
 	return unzipped, nil
 }
 
 // Save compresses and writes a slice of data to disk as an encrypted file.
-func Save(filename, password string, data []byte) (err error) {
-
-	// Zip the plaintext before encrypting.
+func Save(filename, password string, plaintext []byte) error {
+	// Compress the plaintext before encrypting.
 	var zipped bytes.Buffer
 	zipper := gzip.NewWriter(&zipped)
-	_, err = zipper.Write(data)
+
+	_, err := zipper.Write(plaintext)
 	if err != nil {
-		return
+		return fmt.Errorf("failed to zip plaintext: %w", err)
 	}
-	zipper.Close()
+
+	err = zipper.Close()
+	if err != nil {
+		return fmt.Errorf("failed to close zipper: %w", err)
+	}
 
 	// Generate a random salt.
 	salt, err := ironcrypt.RandBytes(SaltLength)
 	if err != nil {
-		return
+		return fmt.Errorf("failed to generate random salt: %w", err)
 	}
 
 	// Use the password and salt to generate a file encryption key.
@@ -79,35 +83,39 @@ func Save(filename, password string, data []byte) (err error) {
 	// Encrypt the data using the key.
 	ciphertext, err := aes.Encrypt(zipped.Bytes(), key)
 	if err != nil {
-		return
+		return fmt.Errorf("failed to encrypt zipped plaintext: %w", err)
 	}
 
 	// Write the salt and ciphertext to a temporary output file.
-	file, err := os.Create(filename + ".new")
+	file, err := os.Create(filename + ".ironclad.temp")
 	if err != nil {
-		return
+		return fmt.Errorf("failed to create output file: %w", err)
 	}
+
 	_, err = file.Write(salt)
 	if err != nil {
-		return
+		return fmt.Errorf("failed to write salt to output file: %w", err)
 	}
+
 	_, err = file.Write(ciphertext)
 	if err != nil {
-		return
+		return fmt.Errorf("failed to write cyphertext to output file: %w", err)
 	}
+
 	file.Close()
 
-	// Delete any older file instance and rename the temporary file.
+	// Delete any existing file instance.
 	if _, err := os.Stat(filename); err == nil {
 		err = os.Remove(filename)
 		if err != nil {
-			return err
+			return fmt.Errorf("failed to delete existing file: %w", err)
 		}
 	}
-	err = os.Rename(filename+".new", filename)
+
+	err = os.Rename(filename+".ironclad.temp", filename)
 	if err != nil {
-		return
+		return fmt.Errorf("failed to rename file: %w", err)
 	}
 
-	return
+	return nil
 }

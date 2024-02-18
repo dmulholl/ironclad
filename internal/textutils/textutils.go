03a53785c447c8207709feef5408930492e8fd0e
Darren Mulholland <dmulholl@tcd.ie>
2024-02-18 13:13:30 +0000

    Add AddSpacer() function

--- internal/textutils/textutils.go
+++ internal/textutils/textutils.go
@@ -17,3 +17,23 @@ func Indent(text, prefix string) string {
 
 	return builder.String()
 }
+
+// Adds a spacer to a string after every nth character.
+func AddSpacer(text, spacer string, n int) string {
+	var builder strings.Builder
+	runes := []rune(text)
+
+	for i := 0; i < len(runes); i += n {
+		if i > 0 {
+			builder.WriteString(spacer)
+		}
+
+		if i+n > len(runes) {
+			builder.WriteString(string(runes[i:]))
+		} else {
+			builder.WriteString(string(runes[i : i+n]))
+		}
+	}
+
+	return builder.String()
+}
--- internal/textutils/textutils_test.go
+++ internal/textutils/textutils_test.go
@@ -52,3 +52,84 @@ func TestIndent(t *testing.T) {
 		})
 	}
 }
+
+func TestAddSpacer(t *testing.T) {
+	testcases := []struct {
+		name   string
+		input  string
+		spacer string
+		n      int
+		want   string
+	}{
+		{
+			name:   "empty input empty spacer",
+			input:  "",
+			spacer: "",
+			n:      3,
+			want:   "",
+		},
+		{
+			name:   "empty input",
+			input:  "",
+			spacer: "--",
+			n:      3,
+			want:   "",
+		},
+		{
+			name:   "input a",
+			input:  "a",
+			spacer: "--",
+			n:      3,
+			want:   "a",
+		},
+		{
+			name:   "input ab",
+			input:  "ab",
+			spacer: "--",
+			n:      3,
+			want:   "ab",
+		},
+		{
+			name:   "input abc",
+			input:  "abc",
+			spacer: "--",
+			n:      3,
+			want:   "abc",
+		},
+		{
+			name:   "input abcd",
+			input:  "abcd",
+			spacer: "--",
+			n:      3,
+			want:   "abc--d",
+		},
+		{
+			name:   "input abcde",
+			input:  "abcde",
+			spacer: "--",
+			n:      3,
+			want:   "abc--de",
+		},
+		{
+			name:   "input abcdef",
+			input:  "abcdef",
+			spacer: "--",
+			n:      3,
+			want:   "abc--def",
+		},
+		{
+			name:   "input abcdefg",
+			input:  "abcdefg",
+			spacer: "--",
+			n:      3,
+			want:   "abc--def--g",
+		},
+	}
+
+	for _, tc := range testcases {
+		t.Run(tc.name, func(t *testing.T) {
+			got := AddSpacer(tc.input, tc.spacer, tc.n)
+			assert.Equal(t, tc.want, got)
+		})
+	}
+}
